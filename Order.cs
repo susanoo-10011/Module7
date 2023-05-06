0d1052611ea86feb13b9bef11d034fb3f8016853
susanoo-10011 <cycahosir@gmail.com>
2023-05-06 15:23:04 +0700

    Выбор продуктов для заказа

--- Order.cs
+++ Order.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +13,7 @@ namespace Module7
     {
         public TDelivery Delivery;
 
-        public int Number;
 
-        public string Description;
         public Order(TDelivery delivery)
         {
             Delivery = delivery;
@@ -23,6 +22,15 @@ namespace Module7
         public void DisplayDeliveryTime()
         {
             Delivery.DeliveryTime();
+        }
+
+        public void DisplayOrderResults( string adress, string phone)
+        {
+            Delivery["Adress"] = adress;
+            Delivery["Phone"] = phone;
+
+            Console.WriteLine($"Вы собираетесь сделать заказ на сумму .\nВ ваш заказ входит: ." +
+                $"\nАдрес доставки {adress}. \nВаш номер телефона {phone}");
         }
 
         
--- Window/MakeOrder.cs
+++ Window/MakeOrder.cs
@@ -1,4 +1,5 @@
-﻿using Module7.Products;
+﻿using Module7.Delivery.Base;
+using Module7.Products;
 using Module7.Products.Base;
 using System;
 using System.Collections.Generic;
@@ -16,9 +17,42 @@ namespace Module7.Window
 
             ProductListMenu productListMenu = new ProductListMenu();
             productListMenu.CreateListProducts();
+            productListMenu.Menu();
 
 
+
+            UserData();
+            Console.ReadKey();
+        }
+
+        private void UserData()
+        {
             Console.WriteLine("Для создания заказа нужно заполнить данные. \n");
+
+            Console.WriteLine("Выберите товары, которые хотите заказать. Указывайте товары через пробел.");
+            string userInputProducts = Console.ReadLine();
+            string[] userProducts = userInputProducts.Split(' ');
+
+            List<int> values = new List<int>();
+
+            for (int i = 0; i < userProducts.Length; i++)
+            {
+                if (int.TryParse(userProducts[i], out int userProduct))
+                {
+                    values.Add(userProduct);
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка: \"{userProducts[i]}\" не является цифрой!");
+                    return;
+                }
+            }
+
+            Console.WriteLine("Вы заказали: ");
+            for (int i = 0; i < values.Count; i++)
+            {
+                Console.WriteLine($"{i}-ый товар: {ProductListMenu.products[i].Name}");
+            }
 
             Console.WriteLine("Выберите тип доставки. 1 - доставка на дом, 2 - доставка в пункт выдачи, 3 - доставка в розничный магазин");
             Console.Write("Тип доставки: ");
@@ -31,26 +65,22 @@ namespace Module7.Window
 
             if (userDelivery == "1")
             {
-                
-
                 Order<HomeDelivery> order = new Order<HomeDelivery>(new HomeDelivery());
                 order.DisplayDeliveryTime();
-
-
+                order.DisplayOrderResults(userAdress, userPhone);
             }
-            else if(userDelivery == "2")
+            else if (userDelivery == "2")
             {
                 Order<PickPointDelivery> order = new Order<PickPointDelivery>(new PickPointDelivery());
                 order.DisplayDeliveryTime();
+                order.DisplayOrderResults(userAdress, userPhone);
             }
-            else if( userDelivery == "3")
+            else if (userDelivery == "3")
             {
                 Order<ShopDelivery> order = new Order<ShopDelivery>(new ShopDelivery());
                 order.DisplayDeliveryTime();
+                order.DisplayOrderResults(userAdress, userPhone);
             }
-
-
-
         }
     }
 }
